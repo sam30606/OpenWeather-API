7ddb2def682f6585d508c942eeab797d9b3df2b5
sam30606 <sam30606@gmail.com>
2022-06-29 02:30:29 +0800

    v1.1

--- src/searchForm.js
+++ src/searchForm.js
@@ -4,12 +4,13 @@ import setLocations from './setLocations.js';
 import presentData from './presentData.js';
 
 const form = getElement('.search-form');
+const searchIcon = getElement('.fa-search');
 
 const input = getElement('[name="Location"]');
 
 // console.log(form, input);
 
-form.addEventListener('keypress', async (e) => {
+form.addEventListener('keypress', (e) => {
   // //   console.log('input', input.value);
   // e.preventDefault();
   // if (e.key === 'Enter') {
@@ -21,10 +22,16 @@ form.addEventListener('keypress', async (e) => {
   // }
   if (e.key === 'Enter') {
     console.log(e);
-    const value = input.value;
-    const baseURL = `https://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=5&appid=9d6dfc926f81e26a8339901dbb287995`;
-    if (value) {
-      await presentData(`${baseURL}`);
-    }
+    sentURL;
   }
 });
+
+const sentURL = async () => {
+  const value = input.value;
+  const baseURL = `https://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=5&appid=9d6dfc926f81e26a8339901dbb287995`;
+  if (value) {
+    await presentData(`${baseURL}`);
+  }
+};
+
+searchIcon.addEventListener('click', sentURL);
